fa00d69d4c2559f7cf0fecf2f929defc565f8cf1
LizClaudia <20203tn128@utez.edu.mx>
2023-11-13 14:06:14 -0600

    settings-navigation

--- lib/config/navigation/settings_stack.dart
+++ lib/config/navigation/settings_stack.dart
@@ -1,1 +1,19 @@
-class 
+import 'package:donaciones/modules/settigns/screens/settigns.dart';
+import 'package:donaciones/modules/settigns/widgets/form-settings.dart';
+import 'package:flutter/material.dart';
+
+class SettingsStack extends StatelessWidget {
+  const SettingsStack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/settings',
+      routes: {
+        '/settings': (context) => Settigns(),
+        '/settings/form-settings': (context) => const FormSettings(),
+      },
+    );
+  }
+}
